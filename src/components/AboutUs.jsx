6ba8110dd82059e4d0906a98065336277163afcf
anulisba <anulisba@gmail.com>
2026-06-07 12:16:07 +0530

    about us link removerd

--- src/components/AboutUs.jsx
+++ src/components/AboutUs.jsx
@@ -55,14 +55,14 @@ const AboutUs = () => {
                         </motion.p>
                     </div>
 
-                    <motion.div className="button-container" initial={{ opacity: 0 }} whileInView={{ opacity: 1 }} transition={{ duration: 0.8, delay: 0.6 }}>
+                    {/* <motion.div className="button-container" initial={{ opacity: 0 }} whileInView={{ opacity: 1 }} transition={{ duration: 0.8, delay: 0.6 }}>
                         <motion.button className="about-us-button" whileTap={{ scale: 0.98 }} onClick={() => navigate('/about-us')}>
                             Our Full Story
                             <svg width="22" height="22" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                                 <path d="M5 12H19M19 12L13 6M19 12L13 18" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
                             </svg>
                         </motion.button>
-                    </motion.div>
+                    </motion.div> */}
                 </div>
 
                 {/* Timeline / Milestones */}
